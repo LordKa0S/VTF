fa63c9599bb75d63f000311ce694d11cdba995d4
sparsh759 <sparsh759@gmail.com>
2019-02-24 13:08:56 +0530

    begin7

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ include("key.php");
 			          <input type="radio" name="candid" value="2">RaGa 
   					  <br>
 			        
-				    <button type ="button" value="Submit" name="submit" class="waves-effect waves-light btn submitbtn" onclick="myfunction()">Submit</button>
+				    <button type ="button" value="Submit" name="submit" class="waves-effect waves-light btn submitbtn" onclick="myfunction(event)">Submit</button>
 				    
 			</form>
 		</div>
