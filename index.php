9bf8c9509335833992da26dae801396cd060d6d6
sparsh759 <sparsh759@gmail.com>
2019-02-24 10:38:44 +0530

    begin4

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script src="https://raw.githubusercontent.com/travist/jsencrypt/master/bin/jsencrypt.min.js"></script>
+    <script src="bin/jsencrypt.min.js"></script>
+
 	<style>
 	body, html {
 	  height: 100%;
