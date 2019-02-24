1e1fb811e004315afd27fa892c9719a76dbf8cb7
sparsh759 <sparsh759@gmail.com>
2019-02-24 12:34:49 +0530

    begin7

--- index.php
+++ index.php
@@ -39,9 +39,10 @@ include("key.php");
 
 		var encrypt = new JSEncrypt();
 
-		console.log("<?php echo $pubKey;?>");
+		//console.log("<?php echo $pubKey;?>");
 
-		var x = "<?php echo $pubKey;?>";
+		// var x = "<?php echo $pubKey;?>";
+		var x = "21332423"
 
 
 		encrypt.setPublicKey(x);
