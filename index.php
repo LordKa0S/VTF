9b3994591074d8ea18f2e2f41bd0a3dac72a2468
sparsh759 <sparsh759@gmail.com>
2019-02-24 10:56:48 +0530

    begin7

--- index.php
+++ index.php
@@ -58,8 +58,9 @@ include("key.php");
     form.submit();
 }
 post("https://us-central1-bytecamp-c915c.cloudfunctions.net/helloWorld", {encr : encrypted, ids = <?php echo $max["tst"];?>});
-alert("hogya");
 	}
+	
+alert("hogya");
 	myfunction();
 	</script>
 		<?php
