28346e4995fe4342e44e735b6a1d2cc8dd9fed05
Kaustubh Badrike <kaustubh.badrike15@siesgst.ac.in>
2019-02-26 01:24:39 +0530

    Organised
    Co-authored-by: Sparsh <sparsh759@gmail.com>
    Co-authored-by: Kartik <kartik70rajesh@yahoo.co.in>

--- auth-server/index.php
+++ auth-server/index.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script src="bin/jsencrypt.min.js"></script>
+    <script src="jsencrypt-master/bin/jsencrypt.min.js"></script>
 
 	<style>
 	body, html {
